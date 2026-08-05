bfd7aff7949ad120c04eb350a33ca97f0b1df379
Biandin17 <104149408+Biandin17@users.noreply.github.com>
2026-08-06 01:00:04 +0200

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://steelheads.ch',
+  site: 'https://biandin17.github.io',
   integrations: [sitemap()]
 });
